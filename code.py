2fd6300efaff2cf43e5c471bf0186d5e6fdb1fde
david <driedell@ucvts.tec.nj.us>
2020-01-13 09:33:39 -0500

    rectangle

--- code.py
+++ code.py
@@ -1,14 +1,23 @@
 import sys
 import pygame
 
+#must call before anything else for other functions to work
 pygame.init()
 
+#creates the display screen with the desired size
 screen = pygame.display.set_mode( (600, 400) )
 
+#set-up the colors based on RGB table
 white = pygame.Color(255,255,255)
+black = pygame.Color(0,0,0)
+red = pygame.Color(255,0,0)
 
+#makes the display screen white
 screen.fill(white)
 pygame.display.flip()
+
+#draws the rectabgle which the player can move
+player = pygame.draw.rect(screen, red, (262.5, 375, 75, 25))
 
 # Main loop. Your game would go inside this loop
 while True:
@@ -18,3 +27,4 @@ while True:
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+    pygame.display.update()
