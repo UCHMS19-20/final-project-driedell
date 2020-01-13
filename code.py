d10df64b5b0131c81c0e0cdace0cabf11f5c5a38
david <driedell@ucvts.tec.nj.us>
2020-01-13 10:26:36 -0500

    moving the player

--- code.py
+++ code.py
@@ -16,15 +16,33 @@ red = pygame.Color(255,0,0)
 screen.fill(white)
 pygame.display.flip()
 
-#draws the rectabgle which the player can move
-player = pygame.draw.rect(screen, red, (262.5, 375, 75, 25))
+#creates variables for player position
+player_x = 262.5
+player_y = 375
 
-# Main loop. Your game would go inside this loop
+#draws the rectabgle which the player can move
+player = pygame.draw.rect(screen, red, (player_x, player_y, 75, 25))
+
+
+# Main loop, game goes inside this loop
 while True:
-    # do something for each event in the event queue (list of things that happen)
+    # do something for each event in the event queue
     for event in pygame.event.get():
         # Check to see if the current event is a QUIT event
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+        #checks to see if current event is a key being pressed
+        elif event.type == pygame.KEYDOWN:
+            #is the key being pressed the left arrow?
+            if event.key == pygame.K_LEFT:
+                #if so move the x positon to the left
+                player_x -= 10
+                screen.blit(player, (player_x, player_y))
+            #is the key being pressed yhe right arrow
+            elif event.key == pygame.K_RIGHT:
+                #if so move the x position to the right
+                player_x += 10
+                screen.blit(player, (player_x, player_y))
+    #redraws the entire screen
     pygame.display.update()
